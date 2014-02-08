1a3c1dd977f8c4377df160c32f64aa065538d3f7
cornernote <cornernote@gmail.com>
2014-02-08 16:58:14 +1030

    finish account update test

--- tests/acceptance/AccountUserControllerUpdateCept.php
+++ tests/acceptance/AccountUserControllerUpdateCept.php
@@ -27,23 +27,61 @@ $I->fillField('AccountLogin_password', 'admin');
 $I->click('Login');
 $I->see('You have successfully logged in.');
 
-// update details
+// update details with empty fields
+$I->amOnPage('/account/user/update');
+$I->fillField('AccountUpdate_first_name', '');
+$I->fillField('AccountUpdate_last_name', '');
+$I->fillField('AccountUpdate_email', '');
+$I->fillField('AccountUpdate_username', '');
+$I->click('Save');
+$I->see('Please fix the following input errors:');
+$I->see('Email cannot be blank.');
+$I->see('Username cannot be blank.');
+$I->see('First Name cannot be blank.');
+
+// update details with invalid email
+$I->amOnPage('/account/user/update');
+$I->fillField('AccountUpdate_first_name', 'admin-first-name');
+$I->fillField('AccountUpdate_last_name', 'admin-last-name');
+$I->fillField('AccountUpdate_email', 'admin-email!mailinator.com');
+$I->fillField('AccountUpdate_username', 'admin-username');
+$I->click('Save');
+$I->see('Please fix the following input errors:');
+$I->see('Email is not a valid email address.');
+
+// update details with valid data
 $I->amOnPage('/account/user/update');
 $I->fillField('AccountUpdate_first_name', 'admin-first-name');
 $I->fillField('AccountUpdate_last_name', 'admin-last-name');
 $I->fillField('AccountUpdate_email', 'admin-email@mailinator.com');
 $I->fillField('AccountUpdate_username', 'admin-username');
 $I->click('Save');
+$I->see('Your account has been updated.');
 
-//
-//// check login
-//$I->amOnPage('/');
-//$I->see('Hello demo');
-//
-//// logout
-//$I->amOnPage('/account/user/logout');
-//$I->see('Your have been logged out.');
-//
-//// check guest
-//$I->amOnPage('/');
-//$I->see('Hello guest');
+// check details
+$I->amOnPage('/account/user/view');
+$I->see('admin-first-name');
+$I->see('admin-last-name');
+$I->see('admin-email@mailinator.com');
+$I->see('admin-username');
+
+// logout
+$I->amOnPage('/account/user/logout');
+$I->see('Your have been logged out.');
+
+// check login
+$I->amOnPage('/account/user/login');
+$I->fillField('AccountLogin_username', 'admin-username');
+$I->fillField('AccountLogin_password', 'admin');
+$I->click('Login');
+$I->see('You have successfully logged in.');
+$I->amOnPage('/');
+$I->see('Hello admin-username');
+
+// logout
+$I->amOnPage('/account/user/logout');
+$I->see('Your have been logged out.');
+
+// check guest
+$I->amOnPage('/');
+$I->see('Hello guest');
