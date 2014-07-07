4898aba17374f738f1cceb7441eeac83c6e116c7
Brett O'Donnell <cornernote@gmail.com>
2014-07-07 12:16:37 +0930

    import all of yiistrap

--- account/AccountModule.php
+++ account/AccountModule.php
@@ -360,7 +360,10 @@ class AccountModule extends CWebModule
             return;
         // setup yiiStrap components
         Yii::setPathOfAlias('bootstrap', realpath($this->yiiStrapPath));
-        Yii::import('bootstrap.helpers.TbHtml');
+        Yii::import('bootstrap.helpers.*');
+        Yii::import('bootstrap.widgets.*');
+        Yii::import('bootstrap.behaviors.*');
+        Yii::import('bootstrap.form.*');
         Yii::app()->setComponents(array(
             'bootstrap' => array(
                 'class' => 'bootstrap.components.TbApi',
